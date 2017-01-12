6dfc49a9391adb3dd01a971818967ea1ae82074f
mfaletti <MM@ADEDEJIs-iMac.local>
2017-01-12 02:54:28 -0500

    minor update

--- app/config/config.js
+++ app/config/config.js
@@ -5,7 +5,6 @@ var cfg = {
 		AWS_BUCKET:"cdn.punditly.com",
 		AWS_CLOUDFRONT_URL:""
 	},
-	staging:{},
   production: {
 		AWS_BUCKET:"cdn.punditly.com",
 		AWS_CLOUDFRONT_URL:""
@@ -22,5 +21,5 @@ exports.env = process.env.NODE_ENV || 'dev';
 exports.cryptoKey = process.env.CRYPTO_KEY || 'Pund1Tly$';
 exports.jwt_secret = process.env.JWT_SECRET || 'Pund1tly';
 exports.mongodb = {
-  uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'localhost/pd'
+  uri: process.env.MONGODB_URI || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'localhost/pd'
 };
